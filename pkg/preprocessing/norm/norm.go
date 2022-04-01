2091be3b57a2ce599eb4381e21b89f6297db64c1
Daniil Novoselov <gudnmail@gmail.com>
2022-04-01 22:21:39 +0500

    feat: improve Norm preprocessing

--- pkg/preprocessing/norm/norm.go
+++ pkg/preprocessing/norm/norm.go
@@ -5,16 +5,25 @@ import (
 	"unicode"
 )
 
+var replaceBySpaces = "-/()*@;:"
+
 func Norm(input string) string {
 	var b strings.Builder
 	splitted := strings.Fields(input)
+	wasSpace := false
 	for i, v := range splitted {
-		if i > 0 {
+		if i > 0 && !wasSpace {
 			b.WriteRune(' ')
+			wasSpace = true
 		}
 		for _, c := range v {
 			if unicode.IsLetter(c) || unicode.IsNumber(c) {
 				b.WriteRune(c)
+				wasSpace = false
+			}
+			if strings.ContainsRune(replaceBySpaces, c) && !wasSpace {
+				b.WriteRune(' ')
+				wasSpace = true
 			}
 		}
 	}
