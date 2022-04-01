3333f8e3e19a402cae5674ea6c49f4034b7ded28
Daniil Novoselov <gudnmail@gmail.com>
2022-04-01 18:23:27 +0500

    feat: add builder.Preprocessed
    This func may be used for build on-fly preprocessing. For example, it
    allow share memory with main storage and calc preprocessing for every
    request

---
+++ pkg/match/builder/preprocessed.go
@@ -0,0 +1,12 @@
+package builder
+
+import "github.com/gudn/vkpredict/pkg/preprocessing"
+
+func Preprocessed(builder Builder, prep preprocessing.Preprocessor) Builder {
+	return func (q string) Scorer {
+		scorer := builder(prep(q))
+		return func(value string) float64 {
+			return scorer(prep(value))
+		}
+	}
+}
